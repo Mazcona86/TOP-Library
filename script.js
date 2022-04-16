c9f1ffd586d70528555e18e2449376d73c201b58
azcon <marceloazcona86@gmail.com>
2022-04-16 12:24:05 -0300

    Code refactor and added missing functionality

--- script.js
+++ script.js
@@ -5,26 +5,27 @@
 let myLibrary = [];
 const addButton = document.querySelector('.section__btn');
 const cardContainer = document.querySelector('.section__card-container');
+const cardBox = document.querySelector('.section__card-box');
 const modal = document.querySelector('.modal');
-const modalSubmit = document.querySelector('.modal__submit-btn');
+const modalSubmit = document.querySelector('.modal__form');
 const overlay = document.querySelector('.overlay');
 const bookTitle = document.getElementById('title');
 const bookAuthor = document.getElementById('author');
 const bookPages = document.getElementById('pages');
 const header = document.querySelector('.header');
-let readBtn = document.querySelector('.section__read-btn');
+const bookIsRead = document.querySelector('#isRead');
+let readBtn = document.querySelectorAll('.section__read-btn');
 let removeBtn = document.querySelectorAll('.section__remove-btn');
 
 /* FUNCTIONS */
 
-function createNewBook() {
+function createNewBook(book) {
   const bookCard = document.createElement('div');
   const title = document.createElement('p');
   const author = document.createElement('p');
   const pages = document.createElement('p');
   const readBtn = document.createElement('button');
   const removeBtn = document.createElement('button');
-  const bookIsRead = document.querySelector('#isRead');
 
   bookCard.className = 'section__card-box';
   title.className = 'book-title';
@@ -33,15 +34,15 @@ function createNewBook() {
   readBtn.className = 'section__read-btn';
   removeBtn.className = 'section__remove-btn';
 
-  title.textContent = `${bookTitle.value}`;
-  author.textContent = `${bookAuthor.value}`;
+  title.textContent = `${book.title}`;
+  author.textContent = `${book.author}`;
   if (bookIsRead.checked) {
     readBtn.textContent = 'Read';
   } else {
     readBtn.textContent = 'Not read';
     readBtn.classList.add('section__read-btn--red');
   }
-  pages.textContent = `${bookPages.value}`;
+  pages.textContent = `${book.pages}`;
   removeBtn.textContent = 'Remove';
 
   bookCard.append(title);
@@ -52,22 +53,38 @@ function createNewBook() {
   cardContainer.append(bookCard);
 
   // Hidding modal after ADD BOOK //
-  modal.classList.add('modal--hidden');
-  overlay.classList.toggle('active');
+  toggleModal();
 }
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
-function removeBook() {
-  removeBtn.forEach(item =>
-    item.addEventListener('click', function (e) {
-      e.target.closest('div').removeBtn();
-    })
-  );
+function toggleModal() {
+  modal.classList.toggle('modal--hidden');
+  overlay.classList.toggle('active');
 }
 
+function mainFunction(e) {
+  e.preventDefault();
+  const book = new Book(
+    bookTitle.value,
+    bookAuthor.value,
+    bookPages.value,
+    bookIsRead.checked
+  );
+  createNewBook(book);
+  addBookToLibrary(book);
+
+  // Updating Nodelist
+  readBtn = document.querySelectorAll('.section__read-btn');
+  removeBtn = document.querySelectorAll('.section__remove-btn');
+
+  book.removeBook();
+  book.toggleRead();
+}
+
+///////////////////////////////////////////////////
 /* CONSTRUCTORS */
 
 function Book(title, author, pages, read) {
@@ -77,22 +94,35 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
+Book.prototype.toggleRead = function () {
+  readBtn.forEach(item =>
+    item.addEventListener('click', function (e) {
+      console.log(e.target);
+      if (e.target.textContent === 'Read') {
+        item.textContent = 'Not Read';
+        this.read = false;
+        item.classList.toggle('section__read-btn--red');
+      } else {
+        item.textContent = 'Read';
+        item.classList.toggle('section__read-btn--red');
+      }
+    })
+  );
+};
+
+Book.prototype.removeBook = function () {
+  removeBtn.forEach(item =>
+    item.addEventListener('click', function (e) {
+      e.target.closest('div').remove();
+    })
+  );
+};
+////////////////////////////////////////////////////////
+
 /* EVENT HANDLERS */
 
 // Adding books
-addButton.addEventListener('click', function () {
-  modal.classList.remove('modal--hidden');
-  overlay.classList.toggle('active');
-});
+addButton.addEventListener('click', toggleModal);
 
 // Submitting book info
-modalSubmit.addEventListener('click', function (e) {
-  e.preventDefault();
-  createNewBook();
-  const book = new Book(bookTitle.value, bookAuthor.value, bookPages.value);
-  addBookToLibrary(book);
-
-  // Updating Nodelist
-  removeBtn = document.querySelectorAll('.section__remove-btn');
-  removeBook();
-});
+modalSubmit.addEventListener('submit', mainFunction);
